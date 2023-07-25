d53cf9173c91a2495b7dcd744578aba8070fdb30
Vamshi N R <126744237+nrvamshi@users.noreply.github.com>
2023-07-25 11:38:28 +0530

    Add files via upload

---
+++ day 6/overiding/A.java
@@ -0,0 +1,9 @@
+package p;
+
+public class A {
+    private int data=40;
+    void display_data()
+    {
+        System.out.println("data value"+data);
+    }
+}
---
+++ day 6/overiding/Simple.java
@@ -0,0 +1,10 @@
+package p;
+
+public class Simple {
+    public static void main(String args[])
+    {
+        A obj=new A();
+        //System.out.println(obj.data);
+        obj.display_data;
+    }
+}
